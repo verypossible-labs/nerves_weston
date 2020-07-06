142264c9ed33d05393ecb23f37680429ad35637e
Justin Schneck <jschneck@mac.com>
2020-07-06 18:00:37 -0400

    Update init code

--- lib/nerves_weston/application.ex
+++ lib/nerves_weston/application.ex
@@ -11,15 +11,17 @@ defmodule NervesWeston.Application do
   def start(_type, _args) do
     opts = Application.get_all_env(:nerves_weston)
 
-    init(opts)
-
     config = opts[:config] || Application.app_dir(:nerves_weston, "priv/weston.ini")
     tty = opts[:tty] || @tty
     extra_args = opts[:extra_args] || []
+    xdg_runtime_dir = opts[:xdg_runtime_dir] || @xdg_runtime_dir
+    env = [{"XDG_RUNTIME_DIR", xdg_runtime_dir}]
+
+    init(xdg_runtime_dir)
 
     children = [
       # Starts a worker by calling: Weston.Worker.start_link(arg)
-      {MuonTrap.Daemon, ["weston", ["--tty=#{tty}", "--config=#{config}"] ++ extra_args]}
+      {MuonTrap.Daemon, ["weston", ["--tty=#{tty}", "--config=#{config}"] ++ extra_args, [env: env]]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
@@ -28,13 +30,11 @@ defmodule NervesWeston.Application do
     Supervisor.start_link(children, opts)
   end
 
-  defp init(opts) do
+  def init(xdg_runtime_dir) do
     :os.cmd('udevd -d');
     :os.cmd('udevadm trigger --type=subsystems --action=add');
     :os.cmd('udevadm trigger --type=devices --action=add');
     :os.cmd('udevadm settle --timeout=30');
-
-    xdg_runtime_dir = opts[:xdg_runtime_dir] || @xdg_runtime_dir
 
     File.mkdir(xdg_runtime_dir)
     stat = File.stat!(xdg_runtime_dir)
